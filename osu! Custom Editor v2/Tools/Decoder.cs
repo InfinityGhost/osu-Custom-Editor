187ee9117f63e07d16b61296471dc71f68b66af5
InfinityGhost <39218853+InfinityGhost@users.noreply.github.com>
2018-12-02 13:35:59 -0500

    Create Decoder.cs

---
+++ osu! Custom Editor v2/Tools/Decoder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace osu__Custom_Editor_v2.Tools
+{
+    public class Decoder
+    {
+        public static int Timestamp(string timestamp)
+        {
+            var a = timestamp.Split(':');
+            var times = a.ToList().ConvertAll(e => Convert.ToInt32(e));
+
+            if (a.Length < 3)
+                throw new FormatException();
+
+            int totalMS = (times[0] * 60000) + (times[1] * 1000) + times[2];
+            return totalMS;
+        }
+    }
+}
