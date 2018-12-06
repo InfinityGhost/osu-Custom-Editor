73f451a45eb9538caad13cbe77c697595286e4d5
InfinityGhost <39218853+InfinityGhost@users.noreply.github.com>
2018-12-06 00:52:10 -0500

    Update namespace.

--- osu! Custom Editor v2/UI Controls/Settings.xaml.cs
+++ osu! Custom Editor v2/UI Controls/Settings.xaml.cs
@@ -13,9 +13,9 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-using OsuBeatmapParser;
-using OsuBeatmapParser.Beatmaps;
-using OsuBeatmapParser.Beatmaps.Sections;
+using OsuParsers;
+using OsuParsers.Beatmaps;
+using OsuParsers.Beatmaps.Sections;
 
 namespace osu__Custom_Editor_v2
 {
